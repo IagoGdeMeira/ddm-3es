09db50e8ab12342f5e9e0b80d07dc80dd32f821d
unknown <iagomeiraifpr@gmail.com>
2024-03-01 19:54:34 -0300

    Cálculo de média | Ex 01

--- 29-02/atividade_01.dart
+++ 29-02/atividade_01.dart
@@ -3,11 +3,15 @@ import 'dart:io';
 void main() {
   var notas = armazenarNotas(2);
 
-  apresentarNotas(notas);
+  var media = calcularMedia(notas);
+  eAprovado(media);
 }
 
-void apresentarNotas(List<double> notas) {
-  for (int i = 0; i < notas.length; i++) print('NOTA ${i + 1} $notas[i]');
+void eAprovado(double media) {
+  if (media >= 6)
+    print('O aluno está aprovado.');
+  else
+    print('O aluno está reprovado.');
 }
 
 List<double> armazenarNotas(int qntNotas) {
@@ -26,9 +30,17 @@ List<double> armazenarNotas(int qntNotas) {
   return notas;
 }
 
+double calcularMedia(List<double> notas) {
+  var media = 0.0;
+
+  for (int i = 0; i < notas.length; i++) media += notas[i];
+
+  return media / notas.length;
+}
+
 double informarNota(int i) {
   print('Informe a ${i + 1}ª nota: ');
-  
+
   var entradaNota = stdin.readLineSync() ?? '-1.0';
   return double.parse(entradaNota);
 }
