a6bf3d7c1d0698962b221af32ac9b42896315b80
IagoGdeMeira <iagomeiraifpr@gmail.com>
2024-10-31 21:50:29 -0300

    Adicionando entidades restantes. Regras de negócio precisam ser definidas.

---
+++ text_e_role/lib/app/domain/entity/chats/game_chat.dart
@@ -0,0 +1,6 @@
+import 'package:text_e_role/app/domain/entity/chats/general_chat.dart';
+
+
+class GameChat extends GeneralChat {
+
+}
---
+++ text_e_role/lib/app/domain/entity/chats/general_chat.dart
@@ -0,0 +1,6 @@
+import 'package:text_e_role/app/domain/entity/chat.dart';
+
+
+class GeneralChat extends Chat{
+  
+}
